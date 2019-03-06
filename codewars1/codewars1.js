6c6f1637330bd350faf6b035f72bf32fdfcbd04d
unknown <christopherlwomackjr@gmail.com>
2019-03-06 17:04:12 -0500

    New problem

---
+++ codewars1/codewars1.js
@@ -0,0 +1,6 @@
+//create a function that takes an integer and makes that integer negative even if it's already negative
+
+function makeNegative(num) {
+  -Math.abs(num)
+  return num
+}
---
+++ codewars1/codewars2.js
@@ -0,0 +1,15 @@
+//write function average which calculates the average of a given list
+//create an array, function adds values of array and divides by the number of elements(length of the array)
+
+var myArray = [];
+
+
+function findAverage (myArray){
+var total=0;
+for(var i = 0; i < myArray.length; i++){
+
+total += myArray[i];
+
+  }
+}
+var avg = total / myArray.length;
