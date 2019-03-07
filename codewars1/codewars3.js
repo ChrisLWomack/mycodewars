9e321ba115e5fb276a4e344382b6c71ba9a4105a
unknown <christopherlwomackjr@gmail.com>
2019-03-06 22:34:45 -0500

    New code wars

---
+++ codewars1/codewars3.js
@@ -0,0 +1,10 @@
+//create a function that removes the first and last character of a string
+
+var str = '';
+function removeChar(str){
+
+var final = str.substr(1).slice(0, -1);
+
+return final
+
+}
