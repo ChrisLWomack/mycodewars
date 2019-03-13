bb7f0f208e046bdb07d8bb66e50907add26829a2
unknown <christopherlwomackjr@gmail.com>
2019-03-13 16:32:31 -0400

    New code Wars

---
+++ codewars1/codewars7.js
@@ -0,0 +1,18 @@
+
+//get volume  of cuboid 
+
+var Kata;
+
+Kata = (function() {
+  function Kata() {}
+
+  Kata.getVolumeOfCuboid = function(length, width, height) {
+    let volume = length * width *height;
+    return volume
+
+    // your code here
+  };
+
+  return Kata;
+
+})();
