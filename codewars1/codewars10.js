ff0792aecd2192358d7b6fff95fcfc2e85603448
unknown <christopherlwomackjr@gmail.com>
2019-03-14 14:53:10 -0400

    New code Wars

---
+++ codewars1/codewars10.js
@@ -0,0 +1,20 @@
+//Your task is to create a function that does four basic mathematical operations.
+//The function should take three arguments - operation(string/char), value1(number), value2(number).
+//The function should return result of numbers after applying the chosen operation.
+function basicOp(operation, value1, value2)
+{
+if(operation==='+'){
+let sum =value1 + value2
+return sum
+}else if(operation==='-'){
+let sum = value1 - value2
+return sum
+}else if(operation==='*'){
+let sum = value1 * value2
+return sum
+}else if(operation==='/'){
+let sum = value1 / value2
+return sum
+}else return 'no'
+
+}
