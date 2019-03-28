e49094a5247c537203a1aba2c090396b144f69a5
unknown <christopherlwomackjr@gmail.com>
2019-03-28 16:18:34 -0400

    New code war

---
+++ codewars1/codewars20.js
@@ -0,0 +1,11 @@
+//create a function that takes a boolean (true or false) and returns respectively string (true or false)
+
+
+function booleanToString(b){
+  if(b===true){
+  return 'true'
+  }else {
+  return 'false'
+  }
+
+}
