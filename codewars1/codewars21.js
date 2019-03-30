9bf23903c82c2de42cbc742e94cde65d4d6728ce
unknown <christopherlwomackjr@gmail.com>
2019-03-30 00:00:14 -0400

    new Code wars

---
+++ codewars1/codewars21.js
@@ -0,0 +1,16 @@
+//Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+
+function replace(s){
+let myArray = s.toArrray
+for(i=0; i<myArray.length; i++){
+if(myArray[i]==='a'||myArray[i]==='e'||myArray[i]==='i'||myArray[i]==='o'||myArray[i]==='u' ){
+i = '!'
+}
+return myArray
+}
+
+
+
+
+}
